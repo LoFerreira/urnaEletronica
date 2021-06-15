47ab0f1bdf4323c692e5f47fc7cbefc6f0390e27
Leonardo Ferreira <leonardo10sp@gmail.com>
2021-06-15 15:11:54 -0300

    criação do etapas.js e finalização do leitor

---
+++ etapas.js
@@ -0,0 +1,46 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: 12345,
+                nome: 'Uzumaki Naruto',
+                partido: 'VILA DA FOLHA',
+                fotos:[{url:'naruto.jpg', legenda: 'Vereador'}]
+            },
+            {
+                numero: 54321,
+                nome: 'Uchiha Madara',
+                partido: 'AKATSUKI',
+                fotos:[{url:'madara.png', legenda: 'Vereador'}]
+            }
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: 12,
+                nome: 'Hatake Kakashi',
+                partido: 'VILA DA FOLHA',
+                vice: 'Maito Gai',
+                fotos:[
+                    {url: 'kakashi.jpg', legenda:'Prefeito'},
+                    {url: 'gai.png', legenda:'Vice-Prefeito', small:true}
+                ]
+            },
+            {
+                numero: 54,
+                nome: 'Uchiha Itachi',
+                partido: 'AKATSUKI',
+                vice: 'Hoshigaki Kisame',
+                fotos:[
+                    {url: 'itachi.jpg', legenda:'Prefeito'},
+                    {url: 'kisame.jpg', legenda:'Vice-Prefeito', small:true}
+                ]
+            }
+        ]
+    }
+]
